97c0a4a25cb6aaa358af81be110481ee11571851
Eric Butler <e.c.butler@outlook.com>
2024-05-28 22:44:02 -0400

    layout experiments

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
-  <div>
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="container h-100">
+    <div class="d-flex h-100" style="padding-top: 50px">
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
+      <div class="bg-primary h-100 overflow-auto" style="min-width: 10rem">
+        <ul>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+          <li>Nav Item</li>
+        </ul>
       </div>
-    </header>
 
-    <RouterView />
+      <main class="flex-fill overflow-auto">
+        <header class="text-center ">
+          <h1>Krypt Pad</h1>
+        </header>
+
+        <RouterView />
+      </main>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style scoped></style>
